5bbde376d742d78635a9efc77f6157ee8d80350c
Adam Richter <draccanos@gmail.com>
2022-07-05 14:25:35 +0200

    add type

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,30 +1,17 @@
 
+import { ProductConnection } from "../schema";
+import fetchApi from "../utils/fetch-api";
 import getAllProductsQuery from "../utils/queries/get-all-products";
 
-type FetchParams = {
-    query: string
+type ReturnType = {
+    products: ProductConnection
 }
 
-const fetchApi = async ({ query }: FetchParams) => {
-  const url = 'http://localhost:4000/graphql';
-
-  const res = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      query,
-    }),
-  });
-  const data = await res.json();
-  return { data };
-};
-
-
-const getAllProducts = async (): Promise<any []> => {
-    const products = await fetchApi({query: getAllProductsQuery});
-    return products.data
+const getAllProducts = async (): Promise<any> => {
+    const {data} = await fetchApi<ReturnType>({
+      query: getAllProductsQuery,
+    });
+    return data.products;
 }
 
 export default getAllProducts
---
+++ framework/shopify/utils/fetch-api.ts
@@ -0,0 +1,29 @@
+type FetcherParams = {
+  query: string;
+};
+
+type FetcherResult<T> = { data: T } 
+
+const fetchApi = async <T>(
+    { query }: FetcherParams
+): Promise<FetcherResult<T>> => {
+  const url = 'http://localhost:4000/graphql';
+
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+    }),
+  });
+  const { data, errors } = await res.json();
+
+  if (errors) {
+    throw new Error(errors[0].message ?? errors.message);
+  }
+  return { data };
+};
+
+export default fetchApi;
