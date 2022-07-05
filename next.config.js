feb4b74a299c682ab438c26e2dabd33fd1ae9365
Adam Richter <draccanos@gmail.com>
2022-07-05 16:03:55 +0200

    merging configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { withFrameworkConfig } = require('./framework/common/config')
 
+/** @type {import('next').NextConfig} */
+const nextConfig = withFrameworkConfig({
+  i18n: {
+    locales: ['en-US', 'es'],
+    defaultLocale: 'en-US',
+  },
+  reactStrictMode: true,
+});
+
+console.log("next.config.js", JSON.stringify(nextConfig, null, 2))
 module.exports = nextConfig
