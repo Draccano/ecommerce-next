eae1829ad8ed32cafc50df716f2e79ba2057d109
Adam Richter <draccanos@gmail.com>
2022-07-22 00:40:34 +0200

    Martue tick moving

--- components/ui/Marquee/Marquee.tsx
+++ components/ui/Marquee/Marquee.tsx
@@ -1,14 +1,34 @@
+import cn from 'classnames'
 import { FC, ReactNode } from 'react'
+import ReactFastMarquee from 'react-fast-marquee'
 import style from './Marquee.module.css'
-
 interface Props {
   children: ReactNode[]
+  direction?: 'left' | 'right'
+  variant?: 'primary' | 'secondary'
+  gradient?: boolean
 }
 
-const Marquee: FC<Props> = ({ children }) => {
+const Marquee: FC<Props> = ({
+  children,
+  direction = 'left',
+  variant = 'primary',
+  gradient = false,
+}) => {
+  const rootClassName = cn(style.root, {
+    [style.secondary]: variant === 'secondary',
+  })
   return (
-    <div className={style.root}>
-      <div className={style.container}>{children}</div>
+    <div className={rootClassName}>
+      <ReactFastMarquee
+        speed={50}
+        gradient={gradient}
+        direction={direction}
+        loop={0}
+        play={true}
+      >
+        <div className={style.container}>{children}</div>
+      </ReactFastMarquee>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home({
           <ProductCard key={product.id} product={product} />
         ))}
       </Grid>
-      <Marquee>
+      <Marquee gradient={false}>
         {products.slice(0, 3).map((product) => (
           <ProductCard variant="slim" key={product.id} product={product} />
         ))}
