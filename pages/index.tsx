973ca086426214edea82b29074835d6db423437b
Adam Richter <draccanos@gmail.com>
2022-07-22 00:50:54 +0200

    marquee secondary variant

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,6 +40,11 @@ export default function Home({
         ))}
       </Grid>
 
+      <Marquee gradient={false} variant="secondary">
+        {products.slice(0, 3).map((product) => (
+          <ProductCard variant="slim" key={product.id} product={product} />
+        ))}
+      </Marquee>
       <Hero
         headline="Arma Reforger is out !"
         description="Marshmallow tart jelly icing cotton candy tootsie roll cotton candy candy canes. Cake liquorice sesame snaps. Cupcake cake cheesecake pie marshmallow lollipop soufflé marshmallow dessert. Cheesecake jujubes halvah chupa chups lollipop tootsie roll. Jelly-o tiramisu jelly toffee cake croissant lemon drops pudding. Donut sesame snaps gummi bears toffee. Sesame snaps jelly-o oat cake chocolate marzipan cake lollipop. Gingerbread cheesecake jujubes fruitcake cake. Tiramisu cotton candy marzipan candy canes oat cake pudding bonbon."
