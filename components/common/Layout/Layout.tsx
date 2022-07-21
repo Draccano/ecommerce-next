5987be5b998a4e4ae19868cb0bb556f1613cd52b
Adam Richter <draccanos@gmail.com>
2022-07-22 00:55:18 +0200

    Footer

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,20 +1,18 @@
-
-import { FC } from "react";
-import style from "./Layout.module.css";
+import { Footer } from '@components/common'
+import { FC } from 'react'
+import style from './Layout.module.css'
 
 interface LayoutProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
- 
 
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className={style.root}>
-      <main className='fit'>
-        {children}
-      </main>
+      <main className="fit">{children}</main>
+      <Footer />
     </div>
-  );
-};
+  )
+}
 
 export default Layout
